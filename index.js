198fe794e1790fdd2924ef68969b5b25aa8f32e8
Akanji Abayomi <akanjiabayomi2@gmail.com>
2026-01-04 16:57:06 +0100

    Zen of javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+const zen = `
+The Zen of JavaScript
+
+Less is more.
+Simple beats clever.
+Explicit beats magical.
+Async beats blocking.
+Composition beats inheritance.
+Readability counts.
+Side effects are dragons.
+`;
+
+module.exports = zen;
